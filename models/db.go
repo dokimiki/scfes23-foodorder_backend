594ea7a5efba306738e47ba9fdff29fdbedee4ec
どきみき <lmznd6yiew@gmail.com>
2023-09-28 01:31:20 +0900

    wip

--- models/db.go
+++ models/db.go
@@ -5,14 +5,15 @@ import (
 )
 
 type Menu struct {
-	ID     uint32 `gorm:"primaryKey"`
+	ID     uint32 `gorm:"primary_key;AUTO_INCREMENT"`
 	Name   string `gorm:"not null"`
 	Price  int    `gorm:"not null"`
-	ImgURL string `gorm:"not null"`
+	ImgUrl string `gorm:"not null"`
+	IsShow bool   `gorm:"not null;default:true"`
 }
 
 type MenuAllergen struct {
-	MenuID uint32 `gorm:"primaryKey"`
+	MenuID uint32 `gorm:"primary_key"`
 	Ebi    string `gorm:"not null"`
 	Kani   string `gorm:"not null"`
 	Komugi string `gorm:"not null"`
@@ -23,46 +24,46 @@ type MenuAllergen struct {
 	Tamago string `gorm:"not null"`
 }
 
-type StoreKeeper struct {
-	ID         uint32 `gorm:"primaryKey"`
-	Token      string `gorm:"unique;not null"`
-	IsApproved bool   `gorm:"not null;default:false"`
-}
-
 type User struct {
-	ID        uint32    `gorm:"primaryKey"`
-	Token     string    `gorm:"unique;not null"`
-	CreatedAt time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
+	ID           uint32    `gorm:"primary_key;AUTO_INCREMENT"`
+	Token        string    `gorm:"unique;not null"`
+	IsInvitation bool      `gorm:"not null;default:false"`
+	IsOrdered    bool      `gorm:"not null;default:false"`
+	InviteCoupon string    `gorm:"not null;default:'none'"`
+	BulkCoupon   string    `gorm:"not null;default:'none'"`
+	CreatedAt    time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
 }
 
-type MenuDetail struct {
-	MenuID    uint32 `gorm:"primaryKey"`
-	Remaining int    `gorm:"not null"`
-	Discount  int    `gorm:"not null;default:0"`
+type ExceptionPotatoOrder struct {
+	ID       uint32 `gorm:"primary_key"`
+	Quantity int    `gorm:"not null;default:0"`
 }
 
 type Order struct {
-	ID               uint32 `gorm:"primaryKey"`
+	ID               uint32 `gorm:"primary_key;AUTO_INCREMENT"`
 	UserID           uint32 `gorm:"not null"`
-	OrderStatus      string `gorm:"not null;default:choosing"`
+	OrderStatus      string `gorm:"not null;default:'choosing'"`
 	NumberTag        int    `gorm:"not null;default:0"`
-	IsCanceled       bool   `gorm:"not null;default:false"`
 	IsPaid           bool   `gorm:"not null;default:false"`
 	IsMobileOrder    bool   `gorm:"not null;default:false"`
-	IsInvitationSent bool   `gorm:"not null;default:false"`
 	TimeOfCompletion time.Time
 	CreatedAt        time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
 }
 
 type OrderItem struct {
-	ID       uint32 `gorm:"primaryKey"`
+	ID       uint32 `gorm:"primary_key;AUTO_INCREMENT"`
 	OrderID  uint32 `gorm:"not null"`
 	MenuID   uint32 `gorm:"not null"`
 	Quantity int    `gorm:"not null"`
 }
 
 type Barcode struct {
-	OrderID     uint32    `gorm:"primaryKey"`
+	OrderID     uint32    `gorm:"primary_key"`
 	BarcodeData string    `gorm:"unique;not null"`
 	CreatedAt   time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
+}
+
+type PotatoDetail struct {
+	ID        uint32 `gorm:"primary_key"`
+	Remaining int    `gorm:"not null"`
 }
--- types/types.go
+++ types/types.go
@@ -1,56 +1,74 @@
 package types
 
-// AllergenContaminationStatus
-type AllergenContaminationStatus string
-
-const (
-    AllergenContaminationStatusNotContains AllergenContaminationStatus = "NotContains"
-    AllergenContaminationStatusContamination AllergenContaminationStatus = "Contamination"
-    AllergenContaminationStatusContains AllergenContaminationStatus = "Contains"
+import (
+	"time"
 )
 
-// AllergensList
 type AllergensList struct {
-    Ebi AllergenContaminationStatus `json:"ebi"`
-    Kani AllergenContaminationStatus `json:"kani"`
-    Komugi AllergenContaminationStatus `json:"komugi"`
-    Kurumi AllergenContaminationStatus `json:"kurumi"`
-    Milk AllergenContaminationStatus `json:"milk"`
-    Peanut AllergenContaminationStatus `json:"peanut"`
-    Soba AllergenContaminationStatus `json:"soba"`
-    Tamago AllergenContaminationStatus `json:"tamago"`
+	Ebi    string `json:"ebi"`
+	Kani   string `json:"kani"`
+	Komugi string `json:"komugi"`
+	Kurumi string `json:"kurumi"`
+	Milk   string `json:"milk"`
+	Peanut string `json:"peanut"`
+	Soba   string `json:"soba"`
+	Tamago string `json:"tamago"`
 }
 
-// MenuItem
+type Coupon struct {
+	Kind string `json:"kind"`
+}
+
+type CouponItemIds struct {
+	None         string `json:"none"`
+	Zero         string `json:"0"`
+	OneHundred   string `json:"100"`
+	TwoHundred   string `json:"200"`
+	ThreeHundred string `json:"300"`
+}
+
 type MenuItem struct {
-    ID string `json:"id"`
-    Name string `json:"name"`
-    Price int `json:"price"`
-    Image string `json:"image"`
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Price  int    `json:"price"`
+	Image  string `json:"image"`
+	IsShow bool   `json:"isShow"`
 }
 
-// CartItem
 type CartItem struct {
-    ID string `json:"id"`
-    Quantity int `json:"quantity"`
+	ID       string `json:"id"`
+	Quantity int    `json:"quantity"`
 }
 
-// Order
 type Order struct {
-    ID string `json:"id"`
-    IsMobileOrder bool `json:"isMobileOrder"`
-    NumberTag int `json:"numberTag"`
-    Items []CartItem `json:"items"`
+	ID            string     `json:"id"`
+	IsMobileOrder bool       `json:"isMobileOrder"`
+	NumberTag     int        `json:"numberTag"`
+	Items         []CartItem `json:"items"`
 }
 
-// OrderedPotato
+type CompleteState struct {
+	State string `json:"state"`
+}
+
+type CompleteInfo struct {
+	Barcode      string     `json:"barcode"`
+	CompleteTime time.Time  `json:"completeTime"`
+	Items        []CartItem `json:"items"`
+}
+
 type OrderedPotato struct {
-    ReceptionTime time.Time
-    CompletionTime time.Time
-    Qty int
-    Order struct {
-        ID string
-        IsMobileOrder bool
-        NumberTag int
-    }
+	ReceptionTime  time.Time `json:"receptionTime"`
+	CompletionTime time.Time `json:"completionTime"`
+	Qty            int       `json:"qty"`
+	Order          Order     `json:"order"`
+}
+
+type User struct {
+	ID        string `json:"id"`
+	IsOrdered bool   `json:"isOrdered"`
+}
+
+type InvitationStatus struct {
+	IsInvited bool `json:"isInvited"`
 }
