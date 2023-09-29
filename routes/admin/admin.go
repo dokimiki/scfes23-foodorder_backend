d8ed35ee86c5aceab60da40a51887c0aadd02ec1
dokimiki <57443812+dokimiki@users.noreply.github.com>
2023-09-29 13:15:16 +0900

    wip

--- routes/admin/admin.go
+++ routes/admin/admin.go
@@ -23,6 +23,9 @@ func GetPotatoData(c echo.Context) error {
 	// レスポンスを作成する
 	response := []types.OrderedPotato{}
 	for _, order := range orders {
+		if (order.MenuID >= 16) {
+			continue;
+		}
 		// DBから注文した商品の数を数えて取得する
 		var orderedItems []models.OrderItem
 		if err := database.DB.Where("order_id = ?", order.ID).Find(&orderedItems).Error; err != nil {
